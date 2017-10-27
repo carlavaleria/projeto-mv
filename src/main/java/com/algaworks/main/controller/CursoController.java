df5fcd53d993f26323be9d5cba9db6625bebb69c
Carla Valéria <carla_empresa@outlook.com>
2017-10-27 18:09:29 -0200

    add implementaçoes

--- src/main/java/com/algaworks/main/controller/CursoController.java
+++ src/main/java/com/algaworks/main/controller/CursoController.java
@@ -1,11 +1,10 @@
 package com.algaworks.main.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.algaworks.main.dao.CursoDAO;
 
-@Controller
+
 public class CursoController {
 	CursoDAO dao;
 	
--- src/test/java/com/algaworks/main/testes/TesteAdicionarCurso.java
+++ src/test/java/com/algaworks/main/testes/TesteAdicionarCurso.java
@@ -1,9 +1,11 @@
 package com.algaworks.main.testes;
 
+import com.algaworks.main.model.Curso;
+
 public class TesteAdicionarCurso {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
