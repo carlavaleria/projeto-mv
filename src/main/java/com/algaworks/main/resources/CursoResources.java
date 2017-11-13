84a6b7f8d6b6ad43dac4854de60f0d6caed0ad51
Carla Valéria <carla_empresa@outlook.com>
2017-11-13 09:23:18 -0300

    Concluido - colocando @Autowired e deixando o mesmo endpoint

--- src/main/java/com/algaworks/main/resources/CursoResources.java
+++ src/main/java/com/algaworks/main/resources/CursoResources.java
@@ -3,6 +3,7 @@ package com.algaworks.main.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,7 +20,14 @@ import com.algaworks.main.model.Curso;
 @CrossOrigin(origins = "http://localhost:4200")
 public class CursoResources {
 	
-	CursoDAO dao = new CursoDAO();
+	private CursoDAO dao;
+	
+	@Autowired
+	public CursoResources(CursoDAO dao){
+		this.dao = dao;
+	}
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public ResponseEntity<List<Curso>> listar(@RequestBody Curso curso) {
 		dao.salvar(curso);
@@ -27,13 +35,11 @@ public class CursoResources {
 		return new ResponseEntity<List<Curso>>(new ArrayList<Curso>(dao.listar(Curso.class)), HttpStatus.OK);
 	}
 	
-
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	  public ResponseEntity<List<Curso>> listar() {
 	    return new ResponseEntity<List<Curso>>(new ArrayList<Curso>(dao.listar(Curso.class)), HttpStatus.OK);
 	}
 
-	
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Curso> buscar(@PathVariable("id") Integer id) throws Exception {
